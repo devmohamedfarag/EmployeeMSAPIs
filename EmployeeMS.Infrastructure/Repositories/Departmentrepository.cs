eb1be3a6da9ec7004713ebaa4169eaaaae566d6e
mohamedfarag22-dot <mohamedfarag2272001@gmail.com>
2025-10-12 15:52:42 +0300

    - Createing Generic Repository Interface and the Generic impelmentaion. - Creating project Repositories that inherits from the generic repositories

---
+++ EmployeeMS.Infrastructure/Repositories/Departmentrepository.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeMS.Domain.Entities;
+using EmployeeMS.Infrastructure.AppDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeMS.Infrastructure.Repositories
+{
+    public class Departmentrepository : GenericRepository<Department>
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public Departmentrepository(ApplicationDbContext dbcontext) : base(dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+    }
+}
