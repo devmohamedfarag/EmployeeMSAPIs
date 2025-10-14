b7a2a7f204afabe734b70c9db3d814fd2aee7254
mohamedfarag22-dot <mohamedfarag2272001@gmail.com>
2025-10-14 22:59:09 +0300

    -Create Migration

---
+++ EmployeeMS.Application/Features/Departments/Commands/CreateDepartmentCommand/CreateDepartmentCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeMS.Application.Features.Departments.Commands.CreateDepartmentCommand
+{
+    internal class CreateDepartmentCommandHandler
+    {
+    }
+}
